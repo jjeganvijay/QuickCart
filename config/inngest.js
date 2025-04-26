87843308e7f17110d597b2e477b969fad004f358
jjeganvijay <jjeganvijay@gmail.com>
2025-04-27 00:23:35 +0530

    First Commit

---
+++ config/inngest.js
@@ -0,0 +1,62 @@
+import User from "@/models/User";
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "quickcart-next" });
+//inngest function to save userdata toa adatabase 
+export const syncUserCreation = inngest.createFunction(
+    {
+        id: 'sync-user-from-clerk'
+    },
+    {
+        event:'clerk/user.created'
+    },
+    async ({event})=>{
+        const {id,first_name,last_name,email_addresses,image_url}=event.data
+        const userData ={
+            _id:id,
+            email:email_addresses[0].email_address,
+            name: first_name +' '+last_name,
+            imageUrl:image_url
+        }
+        await connectDB()
+        await User.create(userData)
+    }
+
+)
+//ingest to update user data in database
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: 'update-user-from-clerk'
+    },
+    {
+        event: 'clerk/user.updated'
+    },
+    async ({event})=>{
+        const {id,first_name,last_name,email_addresses,image_url}=event.data
+        const userData ={
+            _id:id,
+            email:email_addresses[0].email_address,
+            name: first_name +' '+last_name,
+            imageUrl:image_url
+        }
+        await connectDB()
+        await User.findByIdAndUpdate(id,userData)
+    }
+    
+)
+
+// Inngest function to delete user from the dtatabase
+export const SyncUserDeletion=inngest.apiBaseUrl.createFunction(
+    {
+        id: 'delete-user-with-clerk'
+
+    },
+    {
+        event:'clerk/user.deleted'
+    },
+    async ({event})=>{
+        const {id}=event.data
+        await User.findByIdAndDelete(id)
+    }
+)
